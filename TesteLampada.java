51d7973ba9dcba5ff4e2fa184b5cbd8679358bc4
Andrey_Del_Bello <andrey.delbello2002@gmail.com>
2021-11-11 17:52:26 +0000

    Teste Lampada Concluido

---
+++ TesteLampada.java
@@ -0,0 +1,21 @@
+public class TesteLampada{
+
+
+    
+    public static void main(String[] args){
+
+
+
+        Lampada l=new Lampada();
+
+        l.desligar();
+
+        if(l.isSituacaoLampada()){
+            System.out.println("Lampadaligada");
+        }
+        else{
+
+        System.out.println("Lampadadesligada");
+        }
+    }
+}
